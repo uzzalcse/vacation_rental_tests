59ae88558f8e68412ddd1f1992be3627c39a4739
uzzalcse <uzzal.cse42@gmail.com>
2024-12-10 18:18:33 +0600

    unnecessary comments removed 2 times

--- main.py
+++ main.py
@@ -8,9 +8,12 @@ from tests_runners.run_tests import run_test_currency_filtering
 if __name__ == "__main__":
     page_url = "https://www.alojamiento.io/"
 
-    # run_h1_test(page_url)
-    # run_heading_sequence_test(page_url)
-    # run_image_alt_test(page_url)
-    # run_url_test(page_url)   
-    # run_scrape_data(page_url)
+    run_h1_test(page_url)
+    run_heading_sequence_test(page_url)
+    run_image_alt_test(page_url)
+    run_url_test(page_url)   
+    run_scrape_data(page_url)
     run_test_currency_filtering(page_url)
+
+
+
